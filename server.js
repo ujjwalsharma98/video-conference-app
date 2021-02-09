a0a27522b0c78d49d2cc006c18e0d9095996efbb
Ujjwal Sharma <us36335@gmail.com>
2021-02-09 23:02:54 +0530

    completed

--- server.js
+++ server.js
@@ -19,8 +19,13 @@ app.get('/:room', (req, res) => {
 
 
 io.on('connection', socket => {
-    socket.on('join-roon', (roomId, userId) => {
-        console.log(">>>>>", roomId, userId)
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId)
+        socket.to(roomId).broadcast.emit('user-connected', userId)
+
+        socket.on('disconnect', () => {
+            socket.to(roomId). broadcast.emit('user-disconnected', userId)
+        })
     })
 })
 
